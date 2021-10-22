29109091d37546931f80bd322087ff267d57ea3a
Pedro Herrera <pedro.herrera.bazaes@gmail.com>
2021-10-22 18:06:17 -0300

    implementacion de routes

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const faker = require('faker');
+const routerApi = require('./routes');
+
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
@@ -11,40 +14,8 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola soy una nueva ruta');
 });
 
-app.get('/products', (req, res) => {
-  const { size } = req.query;
-  const products = [];
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      Image: faker.image.imageUrl(),
-    });
-  }
-  res.json(products);
-});
+routerApi(app);
 
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    id,
-    name: 'product 2',
-    price: 2000
-  })
-});
-
-app.get('/users', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset
-    });
-  } else {
-    res.send('No hay paramentros');
-  }
-});
 
 
 
---
+++ routes/products.router.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const faker = require('faker');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  const { size } = req.query;
+  const products = [];
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      Image: faker.image.imageUrl(),
+    });
+  }
+  res.json(products);
+});
+
+router.get('/filter', (req, res) => {
+  res.send('Yo soy un filter');
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  if (id === '999') {
+    res.status(404).json({
+      messege: 'not found'
+    });
+  }
+  res.status(200).json({
+    id,
+    name: 'product 2',
+    price: 2000
+  })
+});
+
+router.post('/', (req, res) => {
+  const body = req.body;
+  res.json({
+    massage: "Product created",
+    data: body
+  });
+});
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.status(201).json({
+    massage: "Product updated",
+    data: body,
+    id
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    massage: "Product deleted",
+    id
+  });
+});
+
+module.exports = router;
